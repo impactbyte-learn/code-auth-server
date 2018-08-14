5035a8d92c82009e86616b98e18e22bd6699e8c8
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-14 15:11:10 +0700

    Use check token middleware in users routes

--- api/users/index.js
+++ api/users/index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require('./controller')
+const authController = require('../auth/controller')
 
 router.post('/seed', controller.seed)
 router.post('/register', controller.register)
 router.post('/login', controller.login)
 router.get('/logout', controller.logout)
 
-router.get('/', controller.get)
+router.get('/checktoken', authController.checkToken, authController.get)
+
+router.get('/', authController.checkToken, controller.get)
 router.get('/:id', controller.getOne)
 
 module.exports = router
