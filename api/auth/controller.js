c8c742c123acf267d90fca1f9f958ae485cbada6
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-24 16:22:04 +0700

    Fix try catch in auth controller

--- api/auth/controller.js
+++ api/auth/controller.js
@@ -13,7 +13,7 @@ const controller = {
       req.headers.authorization.split(' ')[0] === 'Bearer'
 
     if (withToken) {
-      const token = req.headers.authorization.split(' ')[1]
+      const token = req.headers.authorization.split(' ')[1] || ''
 
       try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
